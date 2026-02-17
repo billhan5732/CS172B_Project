34e4ad67621530266cfc31348bb43eeef402eb20
Nathan Hai Nguyen <nathannguyen4945@gmail.com>
2026-02-17 14:54:36 -0800

    Preprocessing now sorts slices to main order of scan.

--- src/pre_process_data.py
+++ src/pre_process_data.py
@@ -1,89 +1,66 @@
+from config import EXPORT_NPZ
 import pydicom
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from config import *
 
 class PreProcessData:
-    def __init__(self, importDirectory, importLabels, exportDirectory):
-        self.import_dir = Path(importDirectory)
-        self.export_dir = Path(exportDirectory)
-
-        # read CSV
-        self.df = pd.read_csv(importLabels)
-        print(f"Initial Labels size: {len(self.df)}")
-
-        # Optionally debug / clean
-        if DEBUG:
-            self.debug_clean_labels()
-            print(f"Final Labels size: {len(self.df)}")
-
-        # after cleaning, create arrays
-        self.filenames = self.df['uuid'].to_numpy()
-
+    def __init__(self, import_dir, import_labels, export_dir):
+        self.import_dir = Path(import_dir)
+        self.export_dir = Path(export_dir)
+        self.df = pd.read_csv(import_labels)
         self.labels = self.df['type'].to_numpy()
 
-    def debug_clean_labels(self):
-
-        folder_uuids = {folder.name.strip().lower() for folder in self.import_dir.iterdir() if folder.is_dir()}
-        folder_set = set(folder_uuids)
-
-        filtered_rows = []
-
-        for idx, row in self.df.iterrows():
-            if str(row['uuid']) in folder_set:
-                #print(f"UUID Matched: {row['uuid']}")
-                filtered_rows.append(row)
-
-        df_filtered = pd.DataFrame(filtered_rows).reset_index(drop=True)
-
-        self.df = df_filtered
-
     def run(self):
-        for slicesFolder in self.import_dir.iterdir():
-            if not slicesFolder.is_dir():
+        # Iterate through each folder in the import directory
+        # Each folder is a CT scan comprised of a set of .dcm files
+        for folder in self.import_dir.iterdir():
+            if not folder.is_dir():
                 continue
 
-            export_subdir = self.export_dir / slicesFolder.name
+            # Create export directory
+            export_subdir = self.export_dir / folder.name
             export_subdir.mkdir(parents=True, exist_ok=True)
 
-            if EXPORT_NPZ:
-                npy_slices = []
+            # Get all dcm metadata
+            all_slices = list(folder.glob('*.dcm'))
+            ct_data = list()
+            for dcm in all_slices:
+                ds = pydicom.dcmread(dcm)
+                ct_data.append(ds)
 
-            for slice in slicesFolder.iterdir():
-                if slice.is_dir() or (slice.suffix.lower() != ".dcm"):
-                    continue
+            # NOTE: Must sort slices to maintain correct order of scan
+            ct_data_sorted = sorted(ct_data, key=lambda ds: float(ds.ImagePositionPatient[2]))
 
-                dcm = pydicom.dcmread(slice)
-                pixel_array = dcm.pixel_array.astype(np.float32)
-
-                slope = float(getattr(dcm, "RescaleSlope", 1.0))
-                intercept = float(getattr(dcm, "RescaleIntercept", 0.0))
+            # Iterate through each slice in order, get their hu value, and store in npy_slices
+            npy_slices = []
+            for ds in ct_data_sorted:
+                pixel_array = ds.pixel_array.astype(np.float32)
+                slope = float(getattr(ds, "RescaleSlope", 1.0))
+                intercept = float(getattr(ds, "RescaleIntercept", 0.0))
 
                 hu = pixel_array * slope + intercept
 
+                # Clip HU values to the range [-1000, 400] then scale to [0,1] range
+                # Air ~ 0, Lung Tissue ~0.2-0.3, Soft Tissue ~ 0.7, Bone ~ 1.0
                 hu = np.clip(hu, -1000, 400)
                 slice_image = (hu + 1000) / 1400
-                # print(f"Max: {pixel_array.max()}") # -2048
-                # print(f"Min: {pixel_array.min()}") # 2048
 
-                # print(pixel_array.shape) # 512x512
-                # print(pixel_array.dtype) # int16
+                # # NOTE: Not sure if needed, but previous preprocessing did it so ill keep
+                # # Save individual slice
+                # output_path = export_subdir / (str(ds.InstanceNumber) + ".npy") 
+                # np.save(output_path, slice_image)
 
-                if not EXPORT_NPZ:
-                    # Save each slice as .npy
-                    output_path = export_subdir / (slice.stem + ".npy")
-                    np.save(output_path, slice_image)
-                else:
-                    # Collect slices for later saving
-                    npy_slices.append(slice_image)
+                npy_slices.append(slice_image)
 
-            # After the loop, save the full volume as a single .npz
-            if EXPORT_NPZ:
-                volume_array = np.stack(npy_slices, axis=0)  # shape: (num_slices, H, W)
-                output_path = export_subdir / "volume.npz"
-                np.savez(output_path, volume=volume_array)
+            # Save the full volume as a single .npz
+            volume = np.stack(npy_slices, axis=0)
+            np.savez(export_subdir / "volume.npz", volume=volume)
 
-
-app = PreProcessData("data/Experiment 1 - Blind","data/labels_exp1.csv","data/export")
-app.run()
+if __name__ == "__main__":
+    app = PreProcessData(
+        import_dir="data/Experiment 1 - Blind",
+        import_labels="data/labels_exp1.csv",
+        export_dir="data/export_exp1",
+    )
+    app.run()
